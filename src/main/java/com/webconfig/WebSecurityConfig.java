a1dcd7bebb1fbfb7ac64b901fab8412e6222e1aa
ricofung <ricofungcn@icloud.com>
2018-09-25 15:53:17 +0800

    实现登出

--- src/main/java/com/webconfig/WebSecurityConfig.java
+++ src/main/java/com/webconfig/WebSecurityConfig.java
@@ -1,7 +1,9 @@
 package com.webconfig;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
@@ -9,9 +11,17 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 @EnableOAuth2Sso
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter
 {
+	@Autowired
+	private Environment env;
+	
 	@Override
 	public void configure(HttpSecurity http) throws Exception
 	{
-		http.antMatcher("/**").authorizeRequests().antMatchers("/login**").permitAll().anyRequest().authenticated();
+		http.antMatcher("/**").authorizeRequests().anyRequest().authenticated()
+		.and()
+		.logout().logoutSuccessUrl(env.getProperty("auth-server.logout-uri"))
+        .deleteCookies()
+        .clearAuthentication(true)
+        .invalidateHttpSession(true);
 	}
 }
